eb4fa41a598bd04739f5e4f3793dc4276e0d0572
Simple <Simple@mail.com>
2022-06-19 03:27:01 +0500

    first practice

--- js/script.js
+++ js/script.js
@@ -1,2 +1,25 @@
-const answer = true;
-console.log(!answer);
+"use strict"; //вкл строгий режим
+
+//1
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', ''); //+
+
+//2
+const  personalMovieDB = {
+        count: numberOfFilms,
+        movies: {},
+        actors: {},
+        genres: [],
+        privat: false
+    };
+
+//3 (переменные технические, поэтому используем буквы)
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = +prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = +prompt('На сколько оцените его?', '');
+      
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+   
+console.log(personalMovieDB);
+
